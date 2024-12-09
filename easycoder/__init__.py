0b059e72903b7dfa1c1b12abcc68dabaf54da142
Graham <gt@pobox.com>
2024-12-09 20:39:03 +0000

    v1

--- easycoder/__init__.py
+++ easycoder/__init__.py
@@ -1,3 +1,5 @@
+'''EasyCoder for Python'''
+
 from .ec_classes import *
 from .ec_compiler import *
 from .ec_condition import *
@@ -6,3 +8,5 @@ from .ec_handler import *
 from .ec_program import *
 from .ec_timestamp import *
 from .ec_value import *
+
+__version__ = "1"
